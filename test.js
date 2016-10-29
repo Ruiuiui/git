7ee1d2cd1512d75e2b2f725dc1075a1d485f2d8c
rui <1160295027@qq.com>
2016-10-29 11:43:11 +0800

    add master test

--- test.js
+++ test.js
@@ -2,10 +2,13 @@
 * @Author: anchen
 * @Date:   2016-10-29 11:13:44
 * @Last Modified by:   anchen
-* @Last Modified time: 2016-10-29 11:34:27
+* @Last Modified time: 2016-10-29 11:42:12
 */
 
 'use strict';
 
 
 console.log("这里是UI");
+
+
+console.log("这里究竟是什么？");
