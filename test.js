529bf3f650df8e10dd8165cc8051575722fa2f56
rui <1160295027@qq.com>
2016-10-29 14:59:34 +0800

    restart after noon

--- test.js
+++ test.js
@@ -2,13 +2,10 @@
 * @Author: anchen
 * @Date:   2016-10-29 11:13:44
 * @Last Modified by:   anchen
-* @Last Modified time: 2016-10-29 11:42:12
+* @Last Modified time: 2016-10-29 11:46:30
 */
 
 'use strict';
 
 
 console.log("这里是UI");
-
-
-console.log("这里究竟是什么？");
